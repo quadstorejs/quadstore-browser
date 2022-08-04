252d3f8e5366346daf7f8a1c462d361e438b4bfd
Jacopo Scazzosi <jacopo@scazzosi.com>
2022-08-04 12:39:56 +0200

    uses getStream() in main.js

--- main.js
+++ main.js
@@ -6,10 +6,6 @@ const {
   Engine,
 } = quadstore;
 
-const bindingsToObj = (bindings) => {
-  return Object.fromEntries([...bindings].map(([k, v]) => [k.value, v]));
-};
-
 const main = async () => {
   const dataFactory = new DataFactory();
   console.log('Ok, if we\'re here the browser has loaded everything correctly');
@@ -23,24 +19,25 @@ const main = async () => {
   console.log('We have opened the store');
   await store.clear();
   console.log('We have cleared the store');
-  await store.put(dataFactory.quad(
-    dataFactory.namedNode('http://example.com/theanswer'),
-    dataFactory.namedNode('http://example.com/is'),
-    dataFactory.literal('42', dataFactory.namedNode('https://www.w3.org/2001/XMLSchema#interger')),
-  ));
-  console.log('We have added a new quad');
-  const getResults = await store.get({});
-  console.log('These are all the quads that we have in the store', getResults.items);
+  for (let i = 0; i < 100; i += 1) {
+    await store.put(dataFactory.quad(
+      dataFactory.namedNode(`http://example.com/s${i}`),
+      dataFactory.namedNode('http://example.com/p'),
+      dataFactory.literal(`${i}`, dataFactory.namedNode('https://www.w3.org/2001/XMLSchema#interger')),
+    ));
+  }
+  console.log('We have added 100 quads');
   const queryStr = 'SELECT * {?s ?p ?o}';
   console.log('We are running the following SPARQL query', queryStr);
   const query = await engine.query(queryStr);
   const bindingsStream = await query.execute();
+  let count = 0;
   bindingsStream
     .on('data', (bindings) => {
-      console.log('The SPARQL query has produced a new bindings object', bindingsToObj(bindings));
+      count += 1;
     })
     .on('end', () => {
-      console.log('The SPARQL query has ended');
+      console.log(`The SPARQL query has ended with a count of ${count} matched quads`);
       store.close();
       console.log('We have closed the store');
     })
