d9ef1f8cb2cc67695fcd0e7cdbc2c5816095eeb5
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-05-29 10:04:16 +0200

    testing with rdf-store

--- src/bundle.ts
+++ src/bundle.ts
@@ -3,3 +3,4 @@ export { Quadstore } from 'quadstore';
 export { DataFactory } from 'rdf-data-factory';
 export { Engine } from 'quadstore-comunica';
 export { BrowserLevel } from 'browser-level';
+export { RdfStore } from 'rdf-stores';
